c45213739f47f96eca0c5ded684303add9e64bf3
최현 <hyun@pear-tree.io>
2025-06-26 00:40:02 +0900

    tw

---
+++ Assets/Scripts/SafeArea.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SafeArea : MonoBehaviour
+{
+	public void Awake()
+	{
+		var tra = (RectTransform)transform;
+
+		tra.offsetMin = new(tra.offsetMin.x, Screen.safeArea.yMin);
+		tra.offsetMax = new(-tra.offsetMax.x, -Screen.safeArea.yMax);
+	}
+}
