a6c7b457476f1d212ae886043e00e35f0b3ca3f5
Kyunghwan Sul <kyunghwan789@gmail.com>
2025-06-29 07:29:10 +0900

    tw

--- Assets/Scripts/UIPlacer.cs
+++ Assets/Scripts/UIPlacer.cs
@@ -1,3 +1,4 @@
+﻿using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,13 +10,32 @@ using UnityEngine.UI;
 [RequireComponent(typeof(RectTransform))]
 public class UIPlacer : MonoBehaviour
 {
+    public Canvas canvas;
     public Camera cam;
     [Tooltip("World-space Transform to track")]
     public Transform worldTarget;
 
     void Start()
     {
-        Vector3 pos = cam.WorldToScreenPoint(worldTarget.position);
-        this.transform.position = pos;
+        /*Vector3 pos = cam.WorldToViewportPoint(worldTarget.position);
+        this.transform.position = pos;*/
+
+
+        // 1) get your Canvas’s RectTransform
+        RectTransform canvasRT = canvas.GetComponent<RectTransform>();
+
+        // 2) convert world → screen
+        Vector3 screenPos = cam.WorldToScreenPoint(worldTarget.position);
+
+        // 3) convert screen → local point in the canvas
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvasRT,                // the parent you want coords in
+            screenPos,               // the point in screen‐space
+            canvas.worldCamera,    // your Canvas’s camera
+            out Vector2 localPoint
+        );
+
+        // 4) assign to your UI element’s anchoredPosition
+        this.GetComponent<RectTransform>().anchoredPosition = localPoint;
     }
 }
